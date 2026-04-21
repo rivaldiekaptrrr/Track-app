860abfbf8eaab7f33b9aeb98d4fb75df17b8c104
rivaldisinkoprima <rivaldi.sinkoprima@gmail.com>
2026-04-21 11:26:28 +0700

    fix lint

--- app/src/main/java/com/trackit/app/service/VoiceTileService.kt
+++ app/src/main/java/com/trackit/app/service/VoiceTileService.kt
@@ -24,7 +24,8 @@ class VoiceTileService : TileService() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             startActivityAndCollapse(pendingIntent)
         } else {
-            startActivity(pendingIntent.intentSender)
+            @Suppress("DEPRECATION")
+            startActivityAndCollapse(intent)
         }
     }
 }
