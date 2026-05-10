f0c4f698b4b55ba66d1dfea89d11444e9771ec2c
rivaldiekaptrrr <pbagong75@gmail.com>
2026-05-10 21:29:08 +0700

    Bump version to 1.1.1 for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.trackit.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
