bc857a7cb1df2144b550a1cc2c945a8f34aa8934
rivaldisinkoprima <rivaldi.sinkoprima@gmail.com>
2026-06-17 15:31:09 +0700

    up version to 1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.trackit.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.1.8"
+        versionCode = 8
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
