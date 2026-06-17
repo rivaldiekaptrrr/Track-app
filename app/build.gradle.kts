5cec39748e7bb611acf8f911d3b1dfd64a74e6d6
rivaldisinkoprima <rivaldi.sinkoprima@gmail.com>
2026-06-17 14:46:08 +0700

    chore: bump version 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.trackit.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.6"
+        versionCode = 6
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
